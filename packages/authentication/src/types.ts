4391a5d2fde28fc0f26a1aaae75dd5574bbd6222
dremond <dremond@ca.ibm.com>
2019-04-15 15:28:25 -0400

    chore: added descriptions for interface
    Added a description for name field and authenticate method

--- packages/authentication/src/types.ts
+++ packages/authentication/src/types.ts
@@ -24,11 +24,29 @@ export interface UserProfile {
 }
 
 /**
- * Authenticates user credentials in the given request and returns a 'UserProfile'.
- * If 'authenticate' function fails, it should throw an error.
+ * An interface that describes the common authentication strategy.
+ *
+ * An authentication strategy is a class with an
+ * 'authenticate' method that verifies a user's credentials and
+ * returns the corresponding user profile.
+ *
  */
 export interface AuthenticationStrategy {
+  /**
+   * The 'name' property is a unique identifier for the
+   * authentication strategy ( for example : 'basic', 'jwt', etc)
+   */
   name: string;
+
+  /**
+   * The 'authenticate' method takes in a given request and returns a user profile
+   * which is an instance of 'UserProfile'.
+   * (A user profile is a minimal subset of a user object)
+   * If the user credentials are valid, this method will return a 'UserProfile' instance.
+   * If the user credentials are invalid, this method will throw an error
+   *
+   * @param request
+   */
   authenticate(request: Request): Promise<UserProfile>;
 }
 
